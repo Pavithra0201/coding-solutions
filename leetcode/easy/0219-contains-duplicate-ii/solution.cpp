709c88d671bb2427fb92c3c16ab739155afdd232
Pavithra0201 <pavi020105@gmail.com>
2026-08-31 00:02:09 +0530

    ✅ Contains Duplicate II | Easy | C++ | Runtime: 126 ms (18.28%) | Memory: 134.2 MB | 2026-08-30

---
+++ leetcode/easy/0219-contains-duplicate-ii/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> st;
+        int left=0;
+        int ans;
+        for (int right=0;right<nums.size();right++)
+        {
+            if(st.count(nums[right]))
+            {
+               return true;
+            }
+            st.insert(nums[right]);
+
+            if(right -left ==k)
+            {
+                st.erase(nums[left]);
+                left++;
+            }
+        }
+
+        return false;
+
+        
+    }
+};
