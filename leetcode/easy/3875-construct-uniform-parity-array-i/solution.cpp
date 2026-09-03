479f087d8cab61109cbe7bf6a57599bef6e359f2
Pavithra0201 <pavi020105@gmail.com>
2026-09-03 15:25:36 +0530

    ✅ Construct Uniform Parity Array I | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 30.5 MB | 2026-09-03

--- leetcode/easy/3875-construct-uniform-parity-array-i/solution.cpp
+++ leetcode/easy/3875-construct-uniform-parity-array-i/solution.cpp
@@ -4,17 +4,7 @@ public:
        
         
 
-        for (int i=1;i<nums1.size();i++)
-        {
-            int j=i-1;
-            if (nums1[i] %2  ==0 || (nums1[i]-nums1[j]) %2 ==0)
-                continue;
-            else if (nums1[i] %2  !=0 || (nums1[i]-nums1[j]) %2 !=0)
-                continue;
-            else
-                return false;
-
-        }
+       
 
         return true;
         
