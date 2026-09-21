c9a72bcfd1d5dc71440258e8f30fd995bf450ef1
Pavithra0201 <pavi020105@gmail.com>
2026-09-21 09:48:39 +0530

    ✅ Plus One | Easy | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-21

--- leetcode/easy/0066-plus-one/solution.cpp
+++ leetcode/easy/0066-plus-one/solution.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int size=digits.size();
-      
-        for(int i=size-1;i>=0;i--)
-        {
+        int i=digits.size()-1;
+        
+
             if (digits[i] <9)
                 {digits[i]+=1;
                 return digits;
                 }
-            digits[i]=0;
-        }
-        digits.insert(digits.begin(), 1);
+            else
+            {
+                while(i>=0 && digits[i]>=9)
+                    digits[i]=0;
+                    i--;
+            }
+
+            digits.insert(digits.begin(),1);
+            
+            return digits;
+            
+            
+      
     return digits;
 
         
